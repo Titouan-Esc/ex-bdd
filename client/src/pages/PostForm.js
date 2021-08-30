937bfdde112010584c9ac56c03d19352c169dfe7
Mes Morts <titouanescorneboueu@gmail.com>
2021-08-30 09:44:38 +0200

    Formulaire fait

--- client/src/pages/PostForm.js
+++ client/src/pages/PostForm.js
@@ -3,7 +3,17 @@ import React from 'react'
 const PostForm = () => {
     return (
         <section className="post_form">
-            <h1>Post Form</h1>
+            <form className="formulaire">
+                <div className="contenu">
+                    <label htmlFor="title">Titre du poste</label>
+                    <input type="text" name="title"/>
+                </div>
+                <div className="contenu">
+                    <label htmlFor="content">Contenu</label>
+                    <textarea name="content" cols="30" rows="10"></textarea>
+                </div>
+                <button className="submit">Créer</button>
+            </form>
         </section>
     )
 }
